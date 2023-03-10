a7bf69772817ba2f5d83704e4909a23b86f3d195
dgchoi3904 <dgchoi3904@gmail.com>
2023-03-10 15:39:47 +0900

    기본 태그 틀, 부모자식관계, 스타일셋, 텍스트컨텐츠 배치 80%

--- source/modulefile.js
+++ source/modulefile.js
@@ -1,32 +1,38 @@
 const _EXAMDATA = {
   teamInformation: {
-    teamNumber : 0,
-    teamName: "팀명 작성",
-    teamNameStory: "팀명은 이런이유로 만들었답니다!",
+    teamNumber : 5,
+    teamName: "세동서유",
+    teamNameStory: "세종시, 동구, 서구, 유성구에서 모였기에 팀명은 이런이유로 만들었답니다!",
     teamManager: "공욱재",
     teamAllMember: [
       {
-        memberNumber: 0,
-        name: "공욱재",
+        memberNumber: 1,
+        name: "김동주",
         teamPosition: "팀장",
         githubAddress: "https://www.github.com/"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
+        memberNumber: 2,
+        name: "임지성",
+        teamPosition: "팀원",
         githubAddress: "https://www.github.com/"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
+        memberNumber: 3,
+        name: "최대건",
+        teamPosition: "팀원",
         githubAddress: "https://www.github.com/"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
+        memberNumber: 4,
+        name: "이재권",
+        teamPosition: "팀원",
+        githubAddress: "https://www.github.com/"
+      },
+      {
+        memberNumber: 5,
+        name: "정지은",
+        teamPosition: "팀원",
         githubAddress: "https://www.github.com/"
       },
     ]
